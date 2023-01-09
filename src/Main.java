f29bff42fa7b7d63af84af24057806bdda6763d1
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-01-10 00:46:59 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2 ();
+        task3 ();
+        task4 ();
+        task5 ();
+        task6 ();
+        task7 ();
+        task8 ();
+        task9 ();
+        task10 ();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        for (int a=1; a<11; a++) {
+            System.out.println(a);
+        }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        for (int b=10; b>0; b--) {
+            System.out.println(b);
+        }
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        for (int c=0; c<=17; c=c+2) {
+            System.out.println(c);
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+    }
+    public static void task9 () {
+        System.out.println("Задача 9");
+    }
+    public static void task10 () {
+        System.out.println("Задача 10");
     }
 }
